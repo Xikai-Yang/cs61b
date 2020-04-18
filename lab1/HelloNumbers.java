546e488918376ad7aaba1e6d8570e3c61ef019ec
Your Name <you@example.com>
2020-04-18 13:03:45 +0800

    complete first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;int temp=0;
+        while (x < 10) {
+            temp+=x;System.out.println(temp);
+            x = x + 1;
+        }
+    }
+}
