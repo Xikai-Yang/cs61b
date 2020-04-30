1f56e0c1d29a824ae2112271cfa1875b3ce60be8
Your Name <18810921006@163.com>
2020-04-30 08:24:06 +0800

    proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class TestArrayDequeGold {
+    @Test
+    public void testStudentArrayDeque() {
+        
+        double randomNumber = StdRandom.uniform(4);
+
+    }
+
+}
