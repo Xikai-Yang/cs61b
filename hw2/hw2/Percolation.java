ec7b44816c067cb425de509390b3d7bd4bf473a4
Your Name <18810921006@163.com>
2020-05-07 12:52:01 +0800

    finished hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,90 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int N;
+    private WeightedQuickUnionUF uf;
+    private boolean[][] cache;
+    private int openCount = 0;
+    /**
+     * create N-by-N grid, with all sites initially blocked
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.openCount = 0;
+        this.N = N;
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        cache = new boolean[N][N];
+        for (int i = 1; i <= N ; ++i) {
+            uf.union(0, i);
+        }
+        for (int i = (N-1) * N + 1; i <= N*N ; i++) {
+            uf.union(N * N + 1, i);
+        }
+    }
+
+    private int xyTo1D(int row, int col) {
+        int number = row * N + col;
+        return number + 1;
+    }
+    private void throwException(int row, int col) {
+        if (row >= 0 && row < N && col >= 0 && col < N) {
+            return;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+    private boolean validate(int row, int col) {
+        boolean condition = row >= 0 && row < N && col >=0 && col < N;
+        return condition;
+    }
+    private void unionAll(int row, int col) {
+        int[][] toBeUnioned = {{row - 1, col}, {row + 1, col}, {row, col + 1}, {row, col - 1}};
+        for (int i = 0; i < toBeUnioned.length; i++) {
+            int x = toBeUnioned[i][0];
+            int y = toBeUnioned[i][1];
+            if (validate(x, y) && isOpen(x, y)) {
+                uf.union(xyTo1D(row, col), xyTo1D(x,y));
+            }
+        }
+    }
+
+    /**
+     * open the site (row, col) if it is not open already
+     */
+    public void open(int row, int col) {
+        cache[row][col] = true;
+        unionAll(row, col);
+        openCount++;
+    }
+
+    /**
+     * is the site (row, col) open?
+     */
+    public boolean isOpen(int row, int col) {
+        return cache[row][col];
+    }
+
+    /**
+     * is the site (row, col) full?
+     */
+    public boolean isFull(int row, int col) {
+        return uf.connected(xyTo1D(row, col), 0);
+    }
+
+    /**
+     * number of open sites
+     */
+    public int numberOfOpenSites() {
+        return openCount;
+    }
+
+    /**
+     * does the system percolate?
+     */
+    public boolean percolates() {
+        return uf.connected(0, N * N + 1);
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,44 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private double[] ratioArray;
+    private int times;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <=0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.ratioArray = new double[T];
+        this.times = T;
+        for (int i = 0; i < T; i++) {
+            this.ratioArray[i] = Simulation(N, pf);
+        }
+    }
+
+    private double Simulation(int N, PercolationFactory pf) {
+        Percolation percolation = pf.make(N);
+        while (!percolation.percolates()) {
+            int locationX = StdRandom.uniform(0, N);
+            int locationY = StdRandom.uniform(0, N);
+            percolation.open(locationX, locationY);
+        }
+        double ratio = percolation.numberOfOpenSites() / (N * N);
+        return ratio;
+    }
+    public double mean() {
+        return StdStats.mean(this.ratioArray);
+    }
+    public double stddev() {
+        return StdStats.stddev(this.ratioArray);
+    }
+    public double confidenceLow() {
+        return mean() - (stddev() * 1.96) / Math.sqrt(times);
+    }
+    public double confidenceHigh() {
+        return mean() + (stddev() * 1.96) / Math.sqrt(times);
+    }
 
 }
