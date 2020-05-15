9e8d3c41b6de804414d5ccf6fc2eba081c5eee37
Your Name <18810921006@163.com>
2020-05-15 13:04:43 +0800

    finished lab8 sp19

---
+++ lab8sp19/MyHashMap.java
@@ -0,0 +1,146 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private int initialSize;
+    private double loadFactor;
+    private Set[] buckets;
+    private int size;
+
+    private class Entry {
+        private K key;
+        private V value;
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public K getKey() {
+            return this.key;
+        }
+        public V getValue() {
+            return this.value;
+        }
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+
+    public MyHashMap() {
+        this(DEFAULT_CAPACITY);
+    }
+    public MyHashMap(int initialSize) {
+        this(initialSize, DEFAULT_LOAD_FACTOR);
+    }
+    public MyHashMap(int initialSize, double loadFactor) {
+        this.initialSize = initialSize;
+        this.loadFactor = loadFactor;
+        this.buckets = new HashSet[initialSize];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new HashSet<Entry>();
+        }
+    }
+    private void resize(int capacity) {
+        int threshold = (int) (initialSize * loadFactor);
+        if (size > threshold) {
+            Set[] newBuckets = new HashSet[capacity];
+            for (int i = 0; i < newBuckets.length; i++) {
+                newBuckets[i] = new HashSet<Entry>();
+            }
+            for (int i = 0; i < this.buckets.length; i++) {
+                for (Object node : buckets[i]) {
+                    Entry newNode = (Entry) node;
+                    newBuckets[i].add(new Entry(newNode.getKey(), newNode.getValue()));
+                }
+            }
+        }
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % buckets.length;
+    }
+    @Override
+    public void clear() {
+        this.buckets = new HashSet[initialSize];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new HashSet<Entry>();
+        }
+        this.size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        int hashCode = hash(key);
+        for (Object node : buckets[hashCode]) {
+            Entry entry = (Entry) node;
+            if (entry.getKey().equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        resize(size * 2);
+        int hashCode = hash(key);
+        for (Object node : buckets[hashCode]) {
+            Entry entry = (Entry) node;
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int hashCode = hash(key);
+        for (Object node : buckets[hashCode]) {
+            Entry entry = (Entry) node;
+            if (entry.getKey().equals(key)) {
+                entry.setValue(value);
+                return;
+            }
+        }
+        buckets[hashCode].add(new Entry(key, value));
+        size++;
+    }
+
+    @Override
+    public Set keySet() {
+        Set<K> kSet = new HashSet<>();
+        for (Set set : buckets) {
+            for (Object node : set) {
+                Entry entry = (Entry) node;
+                kSet.add(entry.getKey());
+            }
+        }
+        return kSet;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+
+    @Override
+    public Iterator iterator() {
+        return keySet().iterator();
+    }
+}
